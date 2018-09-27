f7ff9a0e102b860d235efb9382d29ea1dcba4c1e
Rachnerd <rachel_149@hotmail.com>
2018-09-27 08:49:35 +0200

    Added hack comment.

--- src/app/reveal/reveal-page-base.class.ts
+++ src/app/reveal/reveal-page-base.class.ts
@@ -4,6 +4,14 @@ import { RevealService } from './reveal.service';
 export class RevealPage implements OnInit {
 
   constructor(@Inject(ElementRef) elementRef: ElementRef, private revealService: RevealService) {
+    this.hack(elementRef);
+  }
+
+  /**
+   * Reveal expects <section> tags in a specific hierarchy. This hack removes the component selector tag
+   * so that it becomes compatible with Reveal.
+   */
+  hack(elementRef: ElementRef) {
     const ngOnInit = this.ngOnInit;
     this.ngOnInit = () => {
       const element = elementRef.nativeElement;
